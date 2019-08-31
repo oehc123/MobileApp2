c7873c142c24a466cfe658b89c64c8dee0003fb3
oehc123 <josesvz@gmail.com>
2019-08-31 13:55:26 -0400

    adding open fridge button visual

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react'
-import { Header } from 'react-native-elements'
+import { View, StyleSheet } from 'react-native'
+import { Header, Button } from 'react-native-elements'
 
 export default class MainScreen extends Component {
-    static navigationOptions = {
-        header: null
-    };
-    render() {
-        return (
-            <Header
-                leftComponent={{ icon: 'menu', color: '#fff' }}
-                centerComponent={{ text: 'FOODWIZE', style: { color: '#fff' } }}
-                rightComponent={{ icon: 'home', color: '#fff' }}
-                containerStyle={{
-                    backgroundColor: '#7dbf5c',
-                    justifyContent: 'space-around',
-                }}
-            />
-        )
-    }
+  static navigationOptions = {
+    header: null
+  };
+  render() {
+    return (
+      <View>
+				<Header
+					leftComponent={{ icon: 'menu', color: '#fff' }}
+					centerComponent={{ text: 'FOODWIZE', style: { color: '#fff' } }}
+					rightComponent={{ icon: 'home', color: '#fff' }}
+					containerStyle={{
+						backgroundColor: '#7dbf5c',
+						alignContent: 'center',
+						justifyContent: 'space-around',
+					}}
+				/>
+				<Button title="Open Fridge"
+					type='outline' 
+					containerStyle={styles.OpenFridgeBtn}
+				/>
+			</View>
+		);
+	}
 }
+
+const styles = StyleSheet.create({
+  OpenFridgeBtn: {
+    marginTop: 20,
+		alignItems: 'center',
+		borderColor: '#7dbf5c',
+  }
+});
