f83d0d1909f2c3310a7fddf21cd8a77e4c1ebad1
oehc123 <josesvz@gmail.com>
2019-09-01 12:25:22 -0400

    updating splash screen
    1) splash screen set to stay on for one second, and after it will anvigate to the mainscreen

--- App.js
+++ App.js
@@ -1,14 +1,55 @@
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import SplashScreen from './src/screens/SplashScreen';
 import MainScreen from './src/screens/MainScreen';
 
 const AppNavigator = createStackNavigator({
-    Splash: SplashScreen,
     MainScreen: MainScreen
   },
   {
-    initialRouteName: 'Splash',
+    initialRouteName: 'MainScreen',
 });
 
-export default createAppContainer(AppNavigator);
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends React.Component {
+  state = {
+    isReady: false,
+  };
+
+  componentDidMount(){
+    setTimeout( () => {this.setState({ isReady: true })}, 1000)
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return (
+        <View style={styles.container}>
+            <Text style={styles.banner}>Foodwize App</Text>
+        </View>
+      );
+    }
+
+    return (
+      <AppContainer />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7dbf5c',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  banner:{
+    color: '#dbf4a9',
+    fontSize: 24,
+    fontWeight: "bold"
+  },
+  message:{
+    color: '#dbf4a9',
+  },
+});
