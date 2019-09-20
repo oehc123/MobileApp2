064fd3b1527f34d4d62a75f4115f1a96a87c7aff
Roysbert Salinas <rasc.19@gmail.com>
2019-09-20 07:19:59 -0400

    adding features meals carousel

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react'
-import { View, StyleSheet } from 'react-native'
-import { Header, Button } from 'react-native-elements'
+import { View, StyleSheet, Dimensions } from 'react-native'
+import { Header, Button, Image, Text, ScrollView } from 'react-native-elements'
+
+import { Carousel } from '../components/Carousel';
+
+const { width } = Dimensions.get('window');
+const height = width * 0.5
 
 export default class MainScreen extends Component {
   static navigationOptions = {
     header: null
   };
+
+  featuresMeals(item, index){
+    return <Image style={styles.mealImage} source={item.source} />
+  }
+
   render() {
+	const mealImages = [
+		//{ source: { uri: 'https://source.unsplash.com/user/erondu/1600x900', }, },
+		{
+		  source: require('../../assets/images/meal.sample.1.jpg'),
+		},
+		{
+		  source: require('../../assets/images/meal.sample.2.jpg'),
+		},
+		{
+		  source: require('../../assets/images/meal.sample.3.jpg'),
+		},
+	];
     return (
       <View>
 				<Header
@@ -20,13 +42,18 @@ export default class MainScreen extends Component {
 					buttonStyle={styles.OpenFridgeBtn}
 					onPress={ ()=> this.props.navigation.navigate('BarcodeScreen')}
 				/>
-
+				<Carousel 
+            style = { styles.Carousel } 
+            array = { mealImages }
+            widthView = { width }
+            renderView = { this.featuresMeals }/>
 			</View>
 		);
 	}
 }
 
 const styles = StyleSheet.create({
+  
   OpenFridgeBtn: {
 		height: 100,
 		width: 250,
@@ -38,5 +65,9 @@ const styles = StyleSheet.create({
 		backgroundColor: '#7dbf5c',
 		alignContent: 'center',
 		justifyContent: 'space-around',
-	}
+  },
+  mealImage: {
+    width: width,
+    height,
+  },
 });
